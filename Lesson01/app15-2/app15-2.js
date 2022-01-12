750884b16b84b7fb266dba870179f8358c1b3e71
bilguun1324 <b.bilguun1020@gmail.com>
2022-01-12 10:21:25 -0600

    some changes

--- Lesson01/app15-2/app15-2.js
+++ Lesson01/app15-2/app15-2.js
@@ -1,33 +1,55 @@
 const http = require("http");
 const fs = require("fs");
+const { callbackify } = require("util");
 
 let indexFileBuffer;
-let statusCode;
+let statusCode = 200;
+
+const handleFile = function (url, callback) {
+    fs.readFile(__dirname + url, function (err, buffer) {
+        if (err) {
+            indexFileBuffer = "file not found";
+            statusCode = 404;
+            return
+        } else {
+            indexFileBuffer = buffer;
+            statusCode = 200;
+        }
+        callback();
+    });
+}
 
 const serveAllRequests = function (req, res) {
-    switch (req.url) {
-        case "/json":
-            statusCode = 200;
-            res.setHeader("Content-Type", "application/json");
-            res.writeHead(statusCode);
-            res.end("{'message': 'hello world}");
-            break;
-        case "/":
-            fs.readFile(__dirname + "/index.html", function (err, buffer) {
-                if (err) {
-                    indexFileBuffer = "file not found";
-                    statusCode = 404;
-                } else {
-                    indexFileBuffer = buffer;
-                    statusCode = 200;
-                }
-            });
-            res.setHeader("Content-Type", "text/html");
-            res.writeHead(statusCode);
-            res.end(indexFileBuffer);
-            break;
 
-    }
+    console.log("----" + req.url);
+    handleFile(req.url, function () {
+        res.setHeader("Content-Type", "text/html");
+        res.writeHead(statusCode);
+        res.end(indexFileBuffer);
+    });
+    // switch (req.url) {
+    //     case "/json":
+    //         statusCode = 200;
+    //         res.setHeader("Content-Type", "application/json");
+    //         res.writeHead(statusCode);
+    //         res.end("{'message': 'hello world}");
+    //         break;
+    //     case "/":
+    //         fs.readFile(__dirname + "/index.html", function (err, buffer) {
+    //             if (err) {
+    //                 indexFileBuffer = "file not found";
+    //                 statusCode = 404;
+    //             } else {
+    //                 indexFileBuffer = buffer;
+    //                 statusCode = 200;
+    //             }
+    //         });
+    //         res.setHeader("Content-Type", "text/html");
+    //         res.writeHead(statusCode);
+    //         res.end(indexFileBuffer);
+    //         break;
+
+    // }
 
 
 }
@@ -37,16 +59,3 @@ const server = http.createServer(serveAllRequests);
 server.listen(8080, "localhost", function () {
     console.log("Server running");
 })
-// fs.readFile(__dirname + "/index.html", function (err, buffer) {
-//     if (err) {
-//         indexFileBuffer = "file not found";
-//         statusCode = 404;
-//     } else {
-//         indexFileBuffer = buffer;
-//         statusCode = 200;
-//     }
-
-//     server.listen(8080, "localhost", function () {
-//         console.log("Server running");
-//     })
-// });
