429a9b57e0ca96937068b25ca8b20d1145808a0f
bilguun1324 <b.bilguun1020@gmail.com>
2022-01-14 10:52:34 -0600

    app 1

---
+++ Lesson03/app01/api/data/db.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+
+mongoose.connect(process.env.DB_URL);
+
+mongoose.connection.on("connected", function () {
+    console.log("Connnected mongoose " + process.env.DB_URL);
+})
+
+mongoose.connection.on("disconnected", function () {
+    console.log("Disconnected mongoose " + process.env.DB_URL);
+})
+
+mongoose.connection.on("err", function (err) {
+    console.log("Connection err mongoose " + err);
+})
+
+process.on("SIGINT", function () {
+    mongoose.connection.close(function () {
+        console.log(process.env.SIGINT_MSG);
+        process.exit()
+    })
+}) 
