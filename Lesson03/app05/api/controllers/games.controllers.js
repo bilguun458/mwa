2d590e36e94029083ba9bc9ccb9b8d182eb324b5
bilguun1324 <b.bilguun1020@gmail.com>
2022-01-14 14:05:43 -0600

    nested docs

---
+++ Lesson03/app05/api/controllers/games.controllers.js
@@ -0,0 +1,66 @@
+const { ObjectId } = require("mongodb");
+const dbConnection = require("../data/dbConnection")
+const mongoose = require("mongoose")
+
+const Game = mongoose.model(process.env.DB_GAME_MODEL)
+
+getAll = function (req, res) {
+    console.log("GAMES GETALL invoked")
+    let limit = 6;
+    if (req.query && req.query.limit && parseInt(req.query.limit) <= 9) limit = parseInt(req.query.limit);
+
+    Game.find().limit(limit).exec(function (err, games) {
+        console.log("games found");
+        res.status(200).json(games);
+    })
+}
+
+getOne = function (req, res) {
+    console.log("GAMES GETONE invoked")
+    const gameId = req.params.gameId;
+
+    Game.findById(gameId).exec(function (err, game) {
+        console.log("GETONE game found");
+        res.status(200).json(game);
+    })
+}
+
+addOne = function (req, res) {
+    const db = dbConnection.get()
+    const gamesCollection = db.collection(process.env.DB_GAMES_COLLECTION)
+
+    if (req.body && req.body.title && req.body.price && req.body.minPlayers && req.body.minAge) {
+        console.log("Body " + req.body);
+        const newGame = {
+            title: req.body.title,
+            price: parseFloat(req.body.price),
+            minPlayers: parseInt(req.body.minPlayers, 10),
+            minAge: parseInt(req.body.minAge, 10)
+        }
+        gamesCollection.insertOne(newGame, function (err, game) {
+            console.log("insert one");
+            res.status(201).json(game)
+        })
+    } else {
+        console.log("Missing data body ");
+        res.status(400).json({ error: "Data missing POST body" })
+    }
+}
+
+remove = function (req, res) {
+    const db = dbConnection.get()
+    const gamesCollection = db.collection(process.env.DB_GAMES_COLLECTION)
+
+    let gameId = req.params.gameId;
+    gamesCollection.deleteOne({ _id: ObjectId(gameId) }, function (err, games) {
+        console.log("delete one");
+        res.status(200).json({ "message": "delete success" })
+    })
+}
+
+module.exports = {
+    getAll,
+    getOne,
+    addOne,
+    remove,
+}
---
+++ Lesson03/app05/api/routes/index.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const router = express.Router();
+const gamesController = require("../controllers/games.controllers")
+const publisherController = require("../controllers/publisher.controllers")
+const reviewController = require("../controllers/reviews.controllers")
+
+router.route("/games")
+    .get(gamesController.getAll)
+    .post(gamesController.addOne)
+
+router.route("/games/:gameId")
+    .get(gamesController.getOne)
+    .delete(gamesController.remove)
+
+router.route("/games/:gameId/publisher")
+    .get(publisherController.getOne)
+
+router.route("/games/:gameId/reviews")
+    .get(reviewController.getAll)
+
+router.route("/games/:gameId/reviews/:reviewId")
+    .get(reviewController.getOne)
+
+module.exports = router;
